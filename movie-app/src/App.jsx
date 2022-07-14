d0ee6b5ad3104137a8e4d1ccd6ae90d53b8594b8
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-14 16:46:11 +0300

    Movie search using movie api in useEffect

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 
-import './App.css';
+const API_URL = 'http://www.omdbapi.com?apikey=144c5ea3';
 
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies('Godzilla');
+  }
+  , [])
+
   return (
     <div className="App">
     </div>
