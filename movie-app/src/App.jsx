e233bd8a0c403ee3b106b238a38f00f59e69f65b
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-14 17:02:25 +0300

    Created movie card component

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,9 +1,18 @@
 import { useEffect } from 'react';
 
+import MovieCard from './components/MovieCard';
 import searchIcon from './search.svg';
 import './App.css';
 
 const API_URL = 'http://www.omdbapi.com?apikey=144c5ea3';
+
+const movie1 = {
+  "Title": "Godzilla: King of the Monsters",
+  "Year": "2019",
+  "imdbID": "tt3741700",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOGFjYWNkMTMtMTg1ZC00Y2I4LTg0ZTYtN2ZlMzI4MGQwNzg4XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg"
+}
 
 function App() {
 
@@ -21,9 +30,20 @@ function App() {
     <div className="app">
       <h1>MovieNight</h1>
       <div className="searchField">
-          <input type="text" placeholder="Search for a movie" />
-          <img src={searchIcon} alt="search"/>
+          <input type="text" 
+            placeholder="Search for a movie" 
+            value="Godzilla"
+            onChange={() => {}}
+          />
+          <img 
+            src={searchIcon} 
+            alt="search"
+            onclick={() => {}}
+          />
         </div>
+        <div className='container'>
+        <MovieCard movie1={movie1}/>
+      </div>
     </div>
   );
 }
